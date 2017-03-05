21b53a4a5fb60ef89c3f737ff9caea2084ede9b6
Mac <Mac@Mac-PC>
2017-03-05 13:25:26 +0800

    hello

---
+++ Myjava/src/tw/org/iii/Hello.java
@@ -0,0 +1,8 @@
+package tw.org.iii;
+
+public class Hello {
+	public static void main(String[] ming){
+		System.out.println("Hello world");
+		
+	}
+}
---
+++ Myjava/src/tw/org/iii/ming01.java
@@ -0,0 +1,10 @@
+package tw.org.iii;
+
+public class ming01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
