0588c141a2d04ff79940c54faf58fd1b33e03e98
Mac <Mac@Mac-PC>
2017-03-19 17:06:30 +0800

    2017/3/19

--- Myjava/src/tw/org/iii/Turnclass.java
+++ Myjava/src/tw/org/iii/Turnclass.java
@@ -2,10 +2,10 @@ package tw.org.iii;
 
 public class Turnclass {
 	public static void main(String [] args){
-		ming014 m1=new ming014();
+		ming014 m1=new ming015();//實體須先辮子類別才可轉型
 		ming015 m2=(ming015)m1;//轉型
-		ming014 m3=new ming015();
-		ming015 m4=(ming015)m3;//轉型失敗,因為子類別不能互轉型(classcastexception)
+		ming014 m3=new ming014();
+		ming016 m4=(ming016)m3;//轉型失敗,因為子類別不能互轉型(classcastexception)
 	}
 }
 class ming014{
