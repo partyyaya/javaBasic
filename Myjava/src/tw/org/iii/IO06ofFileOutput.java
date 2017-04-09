9f4f67bc9b19484ce89ecaa278f217d3cb5192ba
Mac <Mac@Mac-PC>
2017-04-09 08:54:02 +0800

    2017/04/08

--- Myjava/src/tw/org/iii/IO06ofFileOutput.java
+++ Myjava/src/tw/org/iii/IO06ofFileOutput.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class IO6ofFileOutput {
+public class IO06ofFileOutput {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -14,11 +14,10 @@ public class IO6ofFileOutput {
 		//即使dir1裡沒資料,也不會報錯,因不影響物件實體
 		try {
 			FileOutputStream f1 = new  FileOutputStream(outFile,true);
-			//true代表再後面輸出東西進去(append),預設為false
+			//true代表再後面輸出東西進去(append);預設為false時,每執行一次則會全部刷新,輸出新的
 			//若輸出檔案"找步道位址"則會"新增檔案創立",會先到Buffer緩衝區再決定傳到資料夾裡
 			//若有安全設定則會出例外
-			f1.write(data.getBytes());
-			//每執行一次則會全部刷新,輸出新的
+			f1.write(data.getBytes());			
 			f1.flush();//能準確送到接收端的方法,以免滯留在緩衝區(inputStream沒有)
 			f1.close();
 		} catch (FileNotFoundException e) {
